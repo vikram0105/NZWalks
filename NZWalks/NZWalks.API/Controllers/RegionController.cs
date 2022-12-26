e3634da38d81a3a2521cc27a37d09afd1959367d
Vikram Narayan <viramnarayan05@gmail.com>
2022-12-26 14:14:10 +0530

    End Session Two

---
+++ NZWalks/NZWalks.API/Controllers/RegionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class RegionController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllRegion()
+        {
+            var regions = new List<Region>()
+            {
+                new Region()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellinton",
+                    Code = "WLG",
+                    Area = 227755,
+                    Lat = 1.8822,
+                    Long = 299.88,
+                    Poulation = 500000
+                },
+
+                new Region()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland",
+                    Code = "AUCK",
+                    Area = 227755,
+                    Lat = 1.8822,
+                    Long = 299.88,
+                    Poulation = 500000
+                }
+            };
+
+           return Ok(regions);
+        }
+    }
+}
